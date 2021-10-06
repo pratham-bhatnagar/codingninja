42c3058bc137d597a616b3077b694ae491ec6beb
pratham <prathambhatnagar2803@gmail.com>
2021-10-06 19:05:23 +0530

    done finally

--- AI/Tic_Tac_Toe/tic_tac_toe.py
+++ AI/Tic_Tac_Toe/tic_tac_toe.py
@@ -1,13 +1,13 @@
 import time
 import random
 
-board={
-    1:' ', 2:' ', 3:' ',
+board={                                                            #dict for board 
+    1:' ', 2:' ', 3:' ',    
     4:' ', 5:' ', 6:' ',
     7:' ', 8:' ', 9:' '
 }
 
-def game():
+def game():                                                       # main game loop
     global player
     global bot
     print("#######################################################")
@@ -21,7 +21,7 @@ def game():
     print("\n")
     
     
-    try:
+    try:                                                           
         player=input("What do you wanna play with? ( o / x ): ")
         player.lower()
     
@@ -50,7 +50,7 @@ def game():
     
         
 
-def display(board):
+def display(board):                                     #displays board
     print("\n")
     print(board[1]+' | '+board[2]+' | '+board[3])
     print("--+---+--")
@@ -59,32 +59,23 @@ def display(board):
     print(board[7]+' | '+board[8]+' | '+board[9])
     print("\n")
 
-def spaceIsFree(position):
+def spaceIsFree(position):                              #checks for free space
     if board[position] == ' ':
         return True
     else:
         return False
 
-def insert(player,position):
-    if spaceIsFree(position):
-        board[position]=player
-        display(board)
-        if win_checker():
-            print("%s WINS!")
-        elif draw_checker():
-            print("ITS A DRAW!")
 
 
 
-
-def draw_checker():
+def draw_checker():                                   #checks for draw
     for i in board.keys():
         if board[i] == ' ':
          return False
     
     return True
 
-def win_checker():         
+def win_checker():                                                #checks for winner
     if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
         return True
     elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
@@ -105,9 +96,7 @@ def win_checker():
         return False
 
 
-
-
-def insert(sign,position):
+def insert(sign,position):              #insert user choice into playing board
         board[position]=sign
         time.sleep(1)
         print(display(board))
@@ -122,7 +111,7 @@ def insert(sign,position):
             quit()
         
 
-def player_move():
+def player_move():                      #takes choice from user
 
     try:
         player_position=int(input("Enter the position where you wanna enter: "))
@@ -145,7 +134,7 @@ def player_move():
 
     
 
-def bot_move():
+def bot_move():                         #randomize choice from bot
     bot_position=random.randint(1,9)
     if spaceIsFree(bot_position):
         insert(bot,bot_position)
@@ -159,8 +148,8 @@ def bot_move():
 
 
     
-game()
-while not win_checker():
+game()           
+while not win_checker():          #MAIN GAME LOOP
     bot_move()
     player_move()
     
